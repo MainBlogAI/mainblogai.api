f1053b72ce426d8a0c5849069ec87e4a3d6bcbc9
rennanbaccili <rennanbaccili@gmail.com>
2025-02-20 16:07:44 -0300

    Crianção do model de endereço

---
+++ ApiCatalogo/Models/Address.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace MainBlog.Models
+{
+    [Table("Address")]
+    public class Address
+    {
+        public int Id { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Neighborhood { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+
+        [ForeignKey("Person")]
+        public int PersonId { get; set; }
+
+        [JsonIgnore]
+        public Person Person { get; set; }
+    }
+}
